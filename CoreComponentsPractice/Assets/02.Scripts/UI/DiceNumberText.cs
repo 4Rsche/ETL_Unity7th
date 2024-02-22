2320b7804f51a27059678907e7e13487e9c9a8c0
4Rsche <rlarud788@naver.com>
2024-02-22 12:31:57 +0900

    강사님 주사위 게임
    강사님 주사위 게임

---
+++ CoreComponentsPractice/Assets/02.Scripts/UI/DiceNumberText.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DiceNumberText : MonoBehaviour
+{
+    TMP_Text _diceNumber;
+    void Start()
+    {
+        
+
+    }
+
+    
+}
